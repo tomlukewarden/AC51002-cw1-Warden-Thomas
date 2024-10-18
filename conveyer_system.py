5a4f3ec3b5c2a15bd9b76754b9d6f04e9a227823
tomwarden <tomlukeprogramming@outlook.com>
2024-10-18 13:51:01 +0100

    added a different test file and better working hours logic

--- conveyer_system.py
+++ conveyer_system.py
@@ -1,43 +1,39 @@
 # Start daily production
 def daily_operations():
     def production_start(): 
-        time = int(input('What time is it? '))
-        
-        if time >= 9 and time <= 17:
-            # name = input('Enter your name to set up the productions ')
             start = input("Welcome Back! Would you like to start the production for today? ")
-        else:
-            print('It is not the correct time to start these productions, please come back between 9am and 5pm')
-            exit()
+            
         # Check for valid start responses
-        if start.lower() in ["yes", "y"]:
-            print("Production has begun.")
-            return True
-        elif start.lower() in ["no", "n"]:
-            print("Production will not start.")
-            return False
-        else:  # Error handling
-            print("This is not the correct input.")
-            print("Please input Yes or No.")
-            return production_start()
+            if start.lower() in ["yes", "y"]:
+                print("Production has begun.")
+                return True
+            elif start.lower() in ["no", "n"]:
+                print("Production will not start.")
+                return False
+            else:  # Error handling
+                print("This is not the correct input.")
+                print("Please input Yes or No.")
+                return production_start()
     
     if production_start():
         print('Executing daily productions')
     else:
         print('Daily productions have not started yet')
         
-
-'''
-    
-
-index 10 = 9am
-index 11 = 10am
-index 12 = 11am
-index 13 = 12pm
-index 14 = 1pm
-index 15 = 2pm
-index 16 = 3pm
-index 17 = 4pm
-index 18 = 5pm
-'''
+        working_hours = 0
+        for hour in range(9, 18):
+            print(f"{hour}am, Total working hours so far: {working_hours}")
+        if hour < 17: 
+            working_hours += 1
+        print(f"Total working hours: {working_hours}")
+        
+    def end_of_day():
+        # Want to collect all data from the day and store it in a text file with the date and be able to retrieve this at the start of the next day
+        end = input('Are you ready to perform the end of day tasks? ')
+        if end.lower() in ["yes", "y"]:
+            print('Great, here is your final Report! See you tomorrow!')
+            exit()
+        else:
+            print("Okay, please come back when you are ready ")
+    end_of_day()
 daily_operations()
