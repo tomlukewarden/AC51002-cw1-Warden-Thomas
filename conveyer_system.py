a392b64e7d3b682f86c3f797b8666922a6e75e09
tomwarden <tomlukeprogramming@outlook.com>
2024-10-18 15:50:54 +0100

    Added an end of day text file to add EOD report to

--- conveyer_system.py
+++ conveyer_system.py
@@ -1,5 +1,8 @@
 # Start daily production
 def daily_operations():
+    name = input('Enter your Name')
+    date = input('Enter the Date')
+    
     def production_start():
         start = input(
             "Welcome Back! Would you like to start the production for today? "
@@ -34,15 +37,19 @@ def daily_operations():
         with open('./files/hours.txt', 'r') as hours_file:
             lines = hours_file.readlines()
             total_hours = lines[-1]  # Access the last line (final hour)
+        with open('./files/end_of_day.txt', 'w') as eod_file:
+            eod_file.write(f'{name}\n')
+            eod_file.write(f'{date}\n')
+            eod_file.write(f'{total_hours}\n')
+            eod_file.write('Number of items produced today')
         # Want to collect all data from the day and store it in a text file with the date and be able to retrieve this at the start of the next day
             end = input("Are you ready to perform the end of day tasks? ")
             if end.lower() in ["yes", "y"]:
                 print("Great, here is your final Report: ")
-                print('The Date')
+                print(name)
+                print(date)
                 print(total_hours)
                 print('Number of Items produced')
-                print('Your name')
-                exit()
             else:
                 print("Okay, please come back when you are ready ")
     end_of_day()
