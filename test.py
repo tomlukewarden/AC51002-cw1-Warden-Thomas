26179d4f8e4ab237c926cce0c123e292f1d993e9
tomwarden <tomlukeprogramming@outlook.com>
2024-10-29 11:42:55 +0000

    Thinking about adding a GUI - will not be on main branch unless it works

---
+++ test.py
@@ -0,0 +1,9 @@
+from tkinter import Tk
+
+
+window = Tk() #instantiate an instance of a window
+window.geometry("420x420")
+window.title("Dundee Zest Conveyor Belt System")
+window.config(background="#5cfcff")
+
+window.mainloop()
